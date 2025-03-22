3441d2875451ea62a71b55eb080d9af45100e89e
hamsof <hafizabdulman@gmail.com>
2025-03-22 07:23:54 +0500

    update script

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
 
 (async () => {
-    const browser = await chromium.launch({ headless: true });
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-    await page.goto('https://app.zenhr.com/en/login');
+  await page.goto("https://app.zenhr.com/en/users/sign_in");
 
-    console.log("✅ Check-in completed!");
+  await page.goto("https://app.zenhr.com/en/users/sign_in");
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Email" })
+    .fill("h.abdulmanan@rewaatech.com");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page
+    .getByRole("textbox", { name: "Password" })
+    .fill("Iamstudentofpucit12;");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("button", { name: "Clock-In" }).click();
+  console.log("✅ Check-in completed!");
 
-    await browser.close();
+  await browser.close();
 })();
